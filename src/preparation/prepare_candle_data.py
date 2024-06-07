7705f741e0594bb8403c2acd0eb48adad483f22c
tamas.oarga <tamas.oarga@istructure.com>
2024-06-07 17:37:14 +0200

    Labeling adjustment to define center, inputs, outputs

--- src/preparation/prepare_candle_data.py
+++ src/preparation/prepare_candle_data.py
@@ -13,9 +13,9 @@ def setup():
 
 def prepare_candle_data():
     setup()
-    sliding_window = 60  # 60
-    prediction_window = 5
-    train_window = sliding_window - prediction_window
+    sliding_window = 5  # 60
+    prediction_window = 1
+    input_window = sliding_window - prediction_window
 
     raw_data_location = '../../data/raw/eurusd_m1_candles_2024-01-31_2024-05-08.csv'  # Todo move to environment and/or CLI variable
     ddf = dd.read_csv(
@@ -33,7 +33,7 @@ def prepare_candle_data():
         meta=('timestamp', 'int64')))
 
     # Calculate candle direction 0 -> bullish candle , 1 -> bearish candle
-    ddf = ddf.assign(candle_type=lambda x: x['open'] > x['close'])
+    ddf = ddf.assign(direction=lambda x: x['open'] > x['close'])
     ddf = ddf.replace({False: 0, True: 1})
 
     # To keep time series direction
@@ -45,21 +45,47 @@ def prepare_candle_data():
     ddf = ddf.set_index('timestamp')
 
     # Index is the first timestamp of the moving window
+    print('Shifting candle data to fill the sliding window...')
     ddf_copy = ddf.copy()
     for i in range(1, sliding_window):
         suffix = str(i)
-        print('Exporting forward data. Iteration: ' + suffix)
-        ddf_copy = ddf_copy.join(ddf.shift(int(suffix)), rsuffix=suffix)  # Join shifted versions
+        ddf_copy = ddf_copy.join(ddf.shift(int(suffix)), rsuffix='_'+suffix)  # Join shifted versions
 
     ddf = ddf_copy.dropna()
+
+    # Labeling series with
+    ddf = ddf.rename(columns={
+        'open': 'open_0',
+        'close': 'close_0',
+        'high': 'high_0',
+        'low': 'low_0',
+        'tickvol': 'tickvol_0',
+        'direction': 'direction_0'
+    })
+
+    labels = {}
+    for i in range(0, sliding_window):
+        is_input = i < input_window
+        prefix = 'in' if is_input else 'out'
+        suffix = (input_window - i - 1) if is_input else i - input_window
+        labels['open_' + str(i)] = '{prefix}_open_{suffix}'.format(prefix=prefix, suffix=suffix)
+        labels['close_' + str(i)] = '{prefix}_close_{suffix}'.format(prefix=prefix, suffix=suffix)
+        labels['high_' + str(i)] = '{prefix}_high_{suffix}'.format(prefix=prefix, suffix=suffix)
+        labels['low_' + str(i)] = '{prefix}_low_{suffix}'.format(prefix=prefix, suffix=suffix)
+        labels['tickvol_' + str(i)] = '{prefix}_tickvol_{suffix}'.format(prefix=prefix, suffix=suffix)
+        labels['direction_' + str(i)] = '{prefix}_direction_{suffix}'.format(prefix=prefix, suffix=suffix)
+
+    ddf = ddf.rename(columns=labels)
+
 
     ddf = ddf.compute()
 
     print('Final training set created.')
+    print('Head elements:')
     print(ddf.head(10))
+    print('Tail elements:')
     print(ddf.tail(10))
-    print(len(ddf))
-    print(ddf.dtypes)
+    print('{rows} rows exported'.format(rows= len(ddf)))
 
     ddf.to_csv("training_eurusd_m1.csv")
 
